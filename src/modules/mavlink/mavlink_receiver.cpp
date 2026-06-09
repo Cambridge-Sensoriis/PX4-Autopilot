0944e67d7a68d68322c5d0fb5d68cd437975ea05
Henry Hutchison <henry.hutchison@cambsensoriis.com>
2026-06-09 10:42:18 +0100

    adjust MAVLINK landing target message to send out new landing_target_report UORB instead

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -2709,36 +2709,56 @@ MavlinkReceiver::handle_message_landing_target(mavlink_message_t *msg)
 	mavlink_landing_target_t landing_target;
 	mavlink_msg_landing_target_decode(msg, &landing_target);
 
-	if (landing_target.position_valid && landing_target.frame == MAV_FRAME_LOCAL_NED) {
-		landing_target_pose_s landing_target_pose{};
+	if (landing_target.frame == MAV_FRAME_LOCAL_NED) {
+		landing_target_report_s landing_target_report{};
 
-		landing_target_pose.timestamp = _mavlink_timesync.sync_stamp(landing_target.time_usec);
-		landing_target_pose.abs_pos_valid = true;
-		landing_target_pose.x_abs = landing_target.x;
-		landing_target_pose.y_abs = landing_target.y;
-		landing_target_pose.z_abs = landing_target.z;
+		landing_target_report.timestamp = _mavlink_timesync.sync_stamp(landing_target.time_usec);
+		landing_target_report.frame = landing_target.frame;
+		landing_target_report.target_num = landing_target.target_num;
+		landing_target_report.type = landing_target.type;
+		landing_target_report.distance = landing_target.distance;
 
-		_landing_target_pose_pub.publish(landing_target_pose);
+		for (int i = 0; i < 4; i++) {
+			landing_target_report.q[i] = landing_target.q[i];
+		}
 
-	} else if (landing_target.position_valid) {
+		if (landing_target.position_valid) {
+			landing_target_report.position_valid = true;
+			landing_target_report.pos_x = landing_target.x;
+			landing_target_report.pos_y = landing_target.y;
+			landing_target_report.pos_z = landing_target.z;
+			
+		} else {
+			landing_target_report.position_valid = false;
+			landing_target_report.angle_x = landing_target.angle_x;
+			landing_target_report.angle_y = landing_target.angle_y;
+			landing_target_report.size_x = landing_target.size_x;
+			landing_target_report.size_y = landing_target.size_y;
+		}
+
+		_landing_target_report_pub.publish(landing_target_report);
+
+	} else {
 		// We only support MAV_FRAME_LOCAL_NED. In this case, the frame was unsupported.
 		mavlink_log_critical(&_mavlink_log_pub, "Landing target: coordinate frame %" PRIu8 " unsupported\t",
 				     landing_target.frame);
 		events::send<uint8_t>(events::ID("mavlink_rcv_lnd_target_unsup_coord"), events::Log::Error,
 				      "Landing target: unsupported coordinate frame {1}", landing_target.frame);
 
-	} else {
-		irlock_report_s irlock_report{};
-
-		irlock_report.timestamp = hrt_absolute_time();
-		irlock_report.signature = landing_target.target_num;
-		irlock_report.pos_x = landing_target.angle_x;
-		irlock_report.pos_y = landing_target.angle_y;
-		irlock_report.size_x = landing_target.size_x;
-		irlock_report.size_y = landing_target.size_y;
-
-		_irlock_report_pub.publish(irlock_report);
 	}
+	// HHTODO consider what to do with irlock_report??
+// 	} else {
+// 		irlock_report_s irlock_report{};
+
+// 		irlock_report.timestamp = hrt_absolute_time();
+// 		irlock_report.signature = landing_target.target_num;
+// 		irlock_report.pos_x = landing_target.angle_x;
+// 		irlock_report.pos_y = landing_target.angle_y;
+// 		irlock_report.size_x = landing_target.size_x;
+// 		irlock_report.size_y = landing_target.size_y;
+
+// 		_irlock_report_pub.publish(irlock_report);
+// 	}
 }
 
 void
